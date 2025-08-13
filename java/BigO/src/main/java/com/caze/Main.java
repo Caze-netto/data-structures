68b2e3cde9731b8ef785d370b223f1efe3a59cb1
Valdir Caze Silva Netto <valdir.netto@unimedjp.corp>
2025-08-13 17:32:54 -0300

    lesson1:O(n)

--- java/BigO/src/main/java/com/caze/Main.java
+++ java/BigO/src/main/java/com/caze/Main.java
@@ -1,8 +1,28 @@
 package com.caze;
 
+import java.util.Arrays;
+
 public class Main{
 
     public static void main(String[] args){
 
+        int[] array = new int[10];
+        int cont = 0;
+
+        for(int i=0;i<array.length;i++){
+            array[i]= i;
+        }
+
+        for (int j = 0; j < array.length; j++) {
+            //O(n) independente se tiver 1000 elementos
+            // e tiver que procurar o elemento 423 ainda é O(n)
+            if (array[j] == 9) {
+                System.out.println("Foram " + cont + " elementos para achar o valor: " + array[j]);
+            }else{
+                cont += 1;
+            }
+        }
+
+        System.out.println(Arrays.toString(array));
     }
 }
